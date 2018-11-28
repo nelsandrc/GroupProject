7ed943963d48da524ee0b206b9c6c8b65adb1ed8
nelsandrc <44207981+nelsandrc@users.noreply.github.com>
2018-11-28 10:28:14 -0600

    Add files via upload

---
+++ DVD.java
@@ -0,0 +1,109 @@
+public class DVD {
+    private String movieName;
+    private String leadActor;
+    private String director;
+    private String producer;
+    private String productionCompany;
+    private int currentInventory;
+    private int standardInventory;
+
+    public DVD (String movieName, String  leadActor, String director, String producer, String  productionCompany, int standardInventory){
+        this.movieName = movieName;
+        this.leadActor = leadActor;
+        this.director = director;
+        this.producer = producer;
+        this.productionCompany = productionCompany;
+        this.currentInventory = standardInventory;
+        this.standardInventory = standardInventory;
+    }
+
+    @Override
+    public String toString() {
+        return ("Movie: " + movieName + "\nStar: " + leadActor + "\nDirector: " + director + "\nProducer" + producer + "\nProduction Company: " + productionCompany + "\n");
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof DVD){
+            DVD temp = (DVD) obj;
+            return this.movieName.equals(((DVD) obj).movieName);
+        }
+        else
+            return false;
+    }
+
+    public boolean checkOut(){
+        if(currentInventory > 0){
+            currentInventory--;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public boolean checkIn(){
+        if(currentInventory < standardInventory){
+            currentInventory++;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public int getCurrentInventory() {
+        return currentInventory;
+    }
+
+    public int getStandardInventory() {
+        return standardInventory;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public String getLeadActor() {
+        return leadActor;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public String getProductionCompany() {
+        return productionCompany;
+    }
+
+    public void setCurrentInventory(int currentInventory) {
+        this.currentInventory = currentInventory;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public void setLeadActor(String leadActor) {
+        this.leadActor = leadActor;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public void setProductionCompany(String productionCompany) {
+        this.productionCompany = productionCompany;
+    }
+
+    public void setStandardInventory(int standardInventory) {
+        this.standardInventory = standardInventory;
+    }
+
+}
