e42ee18e341ba4d929a3bb9803c106612cff022d
nelsandrc <44207981+nelsandrc@users.noreply.github.com>
2018-12-05 10:53:48 -0600

    Add files via upload

--- DVD.java
+++ DVD.java
@@ -1,6 +1,6 @@
 /**
  * @author Andrew Nelson
- * Stores information for a dvd for use by a movie rental company
+ * Stores information for a DVD for use by a movie rental company
  */
 public class DVD {
     private String movieName;
@@ -11,6 +11,15 @@ public class DVD {
     private int currentInventory;
     private int standardInventory;
 
+    /**
+     * Creates a new movie object with the given parameters.
+     * @param movieName String representing the title of the movie.
+     * @param leadActor String representing the star of the movie.
+     * @param director String representing the director of the movie.
+     * @param producer String representing the producer of the movie.
+     * @param productionCompany String representing the production company of the movie.
+     * @param standardInventory Integer representing how many copies of the movie the store owns in total.
+     */
     public DVD (String movieName, String  leadActor, String director, String producer, String  productionCompany, int standardInventory){
         this.movieName = movieName;
         this.leadActor = leadActor;
@@ -21,8 +30,19 @@ public class DVD {
         this.standardInventory = standardInventory;
     }
 
+    /**
+     * Creates a new movie with default parameters.
+     */
     public DVD(){
         movieName = "Default Title";
+        leadActor = "N/A";
+        director = "N/A";
+        producer = "N/A";
+        productionCompany = "N/A";
+        currentInventory = standardInventory = 0;
+    }
+    public DVD(String _movieName){
+        movieName =_movieName;
         leadActor = "N/A";
         director = "N/A";
         producer = "N/A";
@@ -45,6 +65,11 @@ public class DVD {
             return false;
     }
 
+
+    /**
+     * Removes one copy of the DVD from the current store inventory
+     * @return Returns true if the system can succesfully check out the DVD.
+     */
     public boolean checkOutDVD(){
         if(currentInventory > 0){
             currentInventory--;
@@ -54,7 +79,11 @@ public class DVD {
             return false;
     }
 
-    public boolean checkIn(){
+    /**
+     * Returns one copy of the DVD to the current store inventory
+     * @return Returns true if the system can succesfully check in the DVD.
+     */
+    public boolean checkInDVD(){
         if(currentInventory < standardInventory){
             currentInventory++;
             return true;
@@ -63,10 +92,18 @@ public class DVD {
             return false;
     }
 
+    /**
+     * Checks if the DVD is currently rented by anyone
+     * @return Returns true if the DVD is checked out.
+     */
     public boolean isRented(){
         return currentInventory < standardInventory;
     }
 
+    /**
+     * Checks if there any copies of the DVD currently in stock.
+     * @return Returns true if there is at least one copy of the DVD in stock.
+     */
     public boolean isInStock(){
         return currentInventory > 0 && standardInventory > 0;
     }
